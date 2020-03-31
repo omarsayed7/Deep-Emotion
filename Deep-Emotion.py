904903d225f2e003409eb7b62068a46830ef1859
omarsayed7 <sayedomar74@gmail.com>
2020-03-31 19:26:03 +0200

    update Deep-Emotion file and remove the dataset class

--- Deep-Emotion.py
+++ Deep-Emotion.py
@@ -44,54 +44,6 @@ epochs = 100
 lr = 0.005
 batchsize = 128
 
-
-class Plain_Dataset(Dataset):
-    def __init__(self,csv_file,img_dir,datatype,transform):
-        '''
-        Documentation
-        NO OneHot encoding
-        '''
-        self.csv_file = pd.read_csv(csv_file)
-        self.lables = self.csv_file['emotion']
-        self.img_dir = img_dir
-        self.transform = transform
-        self.datatype = datatype
-
-    def __len__(self):
-        return len(self.csv_file)
-
-    def __getitem__(self,idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        img = Image.open(self.img_dir+self.datatype+str(idx)+'.jpg')
-        lables = np.array(self.lables[idx])
-        lables = torch.from_numpy(lables).long()
-
-
-        if self.transform :
-            img = self.transform(img)
-
-
-        return img,lables
-
-def eval_data_dataloader(csv_file,img_dir,datatype,sample_number):
-    '''
-    Documentation
-    '''
-
-    transformation = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,),(0.5,))])
-    dataset = Plain_Dataset(csv_file=csv_file,img_dir = img_dir,datatype = datatype,transform = transformation)
-
-    lable = dataset.__getitem__(sample_number)[1]
-    print(lable)
-
-    imgg = dataset.__getitem__(sample_number)[0]
-    imgnumpy = imgg.numpy()
-    imgt = imgnumpy.squeeze()
-    plt.imshow(imgt)
-    plt.show()
-
 class Deep_Emotion(nn.Module):
     def __init__(self):
         '''
