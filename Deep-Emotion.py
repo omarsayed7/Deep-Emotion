6ec0883754a3a683b09cde3f6405c5eb6a839dcc
omarsayed7 <sayedomar74@gmail.com>
2020-03-31 19:33:45 +0200

    modify the archticture of the network and add STN

--- Deep-Emotion.py
+++ Deep-Emotion.py
@@ -58,29 +58,53 @@ class Deep_Emotion(nn.Module):
         self.conv4 = nn.Conv2d(10,10,3)
         self.pool4 = nn.MaxPool2d(2,2)
 
+        self.norm = nn.BatchNorm2d(10)
         #self.dropout = nn.Dropout2d()
 
         self.fc1 = nn.Linear(810,50)
         self.fc2 = nn.Linear(50,7)
 
+        self.localization = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=7),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True),
+            nn.Conv2d(8, 10, kernel_size=5),
+            nn.MaxPool2d(2, stride=2),
+            nn.ReLU(True)
+        )
+
+        self.fc_loc = nn.Sequential(
+            nn.Linear(640, 32),
+            nn.ReLU(True),
+            nn.Linear(32, 3 * 2)
+        )
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+
+    def stn(self, x):
+        xs = self.localization(x)
+        xs = xs.view(-1, 640)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+        return x
+
     def forward(self,input):
-        out = self.conv1(input)
-        out = F.relu(out)
+        out = self.stn(input)
 
+        out = F.relu(self.conv1(out))
         out = self.conv2(out)
-        out = self.pool2(out)
-        out = F.relu(out)
+        out = F.relu(self.pool2(out))
 
-        out = self.conv3(out)
-        out = F.relu(out)
-
-        out = self.conv4(out)
-        out = self.pool4(out)
-        out = F.relu(out)
+        out = F.relu(self.conv3(out))
+        out = self.norm(self.conv4(out))
+        out = F.relu(self.pool4(out))
 
         out = F.dropout(out)
         out = out.view(-1, 810) #####
-        out = self.fc1(out)
+        out = F.relu(self.fc1(out))
         out = self.fc2(out)
 
         return out
